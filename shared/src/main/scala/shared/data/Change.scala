fff44dd87723f996ccf4c64f689548fd2a14de17
Minghao Liu <molikto@gmail.com>
2018-01-29 16:45:05 +0800

    no more rich ref!

--- shared/src/main/scala/shared/data/Change.scala
+++ shared/src/main/scala/shared/data/Change.scala
@@ -6,20 +6,19 @@ import shared.data
 import data.{Node => N}
 import N.{Ref, Content => C}
 
+import Node._
+
 trait ChangeImplicits {
 
   implicit val changeNodePickler: CompositePickler[Change.Node] =
     compositePickler[Change.Node]
       .addConcreteType[Change.Node.Delete]
-      .addConcreteType[Change.Node.NewWithParent]
-      .addConcreteType[Change.Node.NewWithPrevious]
-      .addConcreteType[Change.Node.Move]
+      .addConcreteType[Change.Node.Insert]
 
   implicit val changeContentPickler: CompositePickler[Change.Content] =
     compositePickler[Change.Content]
       .addConcreteType[Change.Content.Insert]
       .addConcreteType[Change.Content.Delete]
-      .addConcreteType[Change.Content.Move]
 
   implicit val changePickler: CompositePickler[Change] =
     compositePickler[Change]
@@ -44,26 +43,13 @@ object Change {
   sealed trait Node extends Change
 
   object Node {
-    case class Delete(node: N.Ref) extends Node() {
+    case class Delete(position: N.Ref) extends Node() {
       override def map(ref: N.Ref): N.Ref = ???
       override def map(ref: N.PointRef): N.PointRef = ???
       override def map(ref: N.SegmentRef): N.SegmentRef = ???
       override def rebase(o: Change): Change = ???
     }
-    case class NewWithParent(parent: N.Ref, id: String) extends Node() {
-      override def map(ref: N.Ref): N.Ref = ???
-      override def map(ref: N.PointRef): N.PointRef = ???
-      override def map(ref: N.SegmentRef): N.SegmentRef = ???
-      override def rebase(o: Change): Change = ???
-    }
-    case class NewWithPrevious(previous: N.Ref, id: String) extends Node() {
-      assert(previous.nonEmpty)
-      override def map(ref: N.Ref): N.Ref = ???
-      override def map(ref: N.PointRef): N.PointRef = ???
-      override def map(ref: N.SegmentRef): N.SegmentRef = ???
-      override def rebase(o: Change): Change = ???
-    }
-    case class Move(node: N.Ref, previous: N.Ref) extends Node() {
+    case class Insert(position: N.Ref, node: N) extends Node() {
       override def map(ref: N.Ref): N.Ref = ???
       override def map(ref: N.PointRef): N.PointRef = ???
       override def map(ref: N.SegmentRef): N.SegmentRef = ???
@@ -74,6 +60,7 @@ object Change {
   sealed trait Content extends Change
   object Content {
     case class Insert(point: N.PointRef, content: C) extends Content() {
+      assert(content.nonEmpty)
       override def map(ref: N.Ref): N.Ref = ???
       override def map(ref: N.PointRef): N.PointRef = ???
       override def map(ref: N.SegmentRef): N.SegmentRef = ???
@@ -85,32 +72,25 @@ object Change {
       override def map(ref: N.SegmentRef): N.SegmentRef = ???
       override def rebase(o: Change): Change = ???
     }
-    case class Move(segment: N.SegmentRef, to: N.PointRef) extends Content() {
-      override def map(ref: N.Ref): N.Ref = ???
-      override def map(ref: N.PointRef): N.PointRef = ???
-      override def map(ref: N.SegmentRef): N.SegmentRef = ???
-      override def rebase(o: Change): Change = ???
-    }
   }
 
-  def apply(root: data.Node, change: Change): data.Node = {
+  /**
+    * @return return a pair of new root, and a reverse change
+    */
+  def apply(root: data.Node, change: Change): (data.Node, Change) = {
     change match {
-      case n: Node.NewWithParent =>
-        root.map(n.parent) { item =>
-          item.copy(childs = N.empty(n.id) +: item.childs)
-        }
-      case n: Node.NewWithPrevious =>
-        root.map(n.previous.dropRight(1)) { item =>
-          val index = n.previous.last + 1
-          item.copy(childs = item.childs.take(index) ++ Seq(N.empty(n.id)) ++ item.childs.drop(index))
-        }
+      case i: Node.Insert =>
+        (root.insert(i.position, i.node), Change.Node.Delete(i.position))
       case d: Node.Delete =>
-        root.map(d.node.dropRight(1)) { item =>
-          val index = d.node.last
-          item.copy(childs = item.childs.take(index) ++ item.childs.drop(index + 1))
-        }
-      case _ =>
-        ???
+        val (node, content) = root.delete(d.position)
+        (node, Change.Node.Insert(d.position, content))
+      case c: Content.Delete =>
+        val (node, content) = root.delete(c.segment)
+        (node, Change.Content.Insert(c.segment.from, content))
+      case c: Content.Insert =>
+        (root.insert(c.point, c.content), Change.Content.Delete(c.point.to(c.content.length)))
+      case Id =>
+        (root, Id)
     }
   }
 
--- shared/src/main/scala/shared/data/Document.scala
+++ shared/src/main/scala/shared/data/Document.scala
@@ -1,30 +1,120 @@
 package shared.data
 
+import shared.data.Node.{Content, Ref}
+
 import scala.util.{Random, Try}
+
 
 object Node extends IdGenerator  {
 
-  // a reference of a child node
-  type Ref = Seq[Int] // empty path = root
+  case class Ref(v: Seq[Int]) {
+    def disjoint(to: Ref): Boolean = !to.v.startsWith(v) && !v.startsWith(to.v)
+    def intersect(to: Ref): Boolean = !disjoint(to)
+    def parent: Ref = Ref(v.dropRight(1))
+    def withChild(a: Int): Ref = Ref(v :+ a)
+    def previous: Ref = parent.withChild(v.last - 1)
+    def next:Ref = parent.withChild(v.last + 1)
+    def head: Int = v.head
+    def tail = Ref(v.tail)
+    def last: Int = v.last
+  }
+
   object Ref {
-    def root = Seq.empty
+    def root = Ref(Seq.empty)
   }
   type Content = String
   object Content {
+    def insert(content: Content, contentPoint: PointRef, c: Content): Content = {
+      content.substring(0, contentPoint) ++ c ++ content.substring(contentPoint)
+    }
+
+    def delete(content: Content, contentSegment: SegmentRef): (Content, Content) = {
+      val res = content.substring(0, contentSegment.from) ++ content.substring(contentSegment.to + 1)
+      val deleted = content.substring(contentSegment.from, contentSegment.to + 1)
+      (res, deleted)
+    }
+
     type PointRef = Int
-    case class SegmentRef(from: PointRef, to: PointRef)
+
+    /**
+      * @param from
+      * @param to inclusive
+      */
+    case class SegmentRef(from: PointRef, to: PointRef) {
+      assert(to >= from)
+    }
     def empty = ""
   }
 
-  case class PointRef(child: Node.Ref, contentPoint: Content.PointRef)
-  case class SegmentRef(child: Node.Ref, contentSegment: Content.SegmentRef)
+  case class PointRef(child: Node.Ref, contentPoint: Content.PointRef) {
+    def to(length: Int): SegmentRef = SegmentRef(child, Content.SegmentRef(contentPoint, contentPoint + length - 1))
+
+  }
+
+  case class SegmentRef(child: Node.Ref, contentSegment: Content.SegmentRef) {
+    def from: PointRef = PointRef(child, contentSegment.from)
+    def to: PointRef = PointRef(child, contentSegment.to)
+  }
 
   def empty(id: String) = Node(id, "", Seq.empty)
 }
 
-case class Node(id: String, content: Node.Content, childs: Seq[Node]) {
+case class Node(id: String, content: Node.Content, childs: Seq[Node]) extends  (Node.Ref => Node) {
+
   // This might fail
+
+  override def apply(child: Node.Ref): Node = {
+    if (child == Node.Ref.root) {
+      this
+    } else {
+      childs(child.head)(child.tail)
+    }
+  }
+
+  /**
+    * @return pair of current node with child removed, and the removed node content
+    */
+  def delete(child: Node.Ref): (Node, Node) = {
+    var removed: Node = null
+    val res = map(child.parent) { item =>
+      val index = child.last
+      removed = item.childs(index)
+      item.copy(childs = item.childs.take(index) ++ item.childs.drop(index + 1))
+    }
+    (res, removed)
+  }
+
+  def delete(child: Node.SegmentRef): (Node, Node.Content) = {
+    var removed: Node.Content = null
+    val res = map(child.child) { item =>
+      val pair = Node.Content.delete(item.content, child.contentSegment)
+      removed = pair._2
+      item.copy(content = pair._1)
+    }
+    (res, removed)
+  }
+
+
+  def insert(point: Node.PointRef, content: Content): Node = {
+    map(point.child) { item =>
+      val cc = Node.Content.insert(item.content, point.contentPoint, content)
+      item.copy(content = cc)
+    }
+  }
+
+  import Node._
+  /**
+    * @return new node
+    */
+  def insert(p: Node.Ref, node: Node): Node = {
+    map(p.parent) { item =>
+      val index = p.last
+      item.copy(childs = item.childs.take(index) ++ Seq(node) ++ item.childs.drop(index))
+    }
+  }
+
   def map(child: Node.Ref)(p: Node => Node): Node = {
+
     if (child == Node.Ref.root) {
       p(this)
     } else {
