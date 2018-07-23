a4563b7f6a3cbece43ebf40a4307fbfacefee7cd
Minghao Liu <molikto@gmail.com>
2018-07-23 18:29:38 +0800

    no message

--- remote_server.sh
+++ remote_server.sh
@@ -1,7 +1,7 @@
-PORT_NUMBER=8080
-lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
 git stash
 git pull
 sbt assembly
 cd jvm
+PORT_NUMBER=8080
+lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
 java -jar target/scala-2.12/server-assembly-0.1.0-SNAPSHOT.jar &
