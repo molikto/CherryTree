a7851e5e9f33fa225f4efd714e8865c10aed160b
Minghao Liu <molikto@gmail.com>
2018-08-23 22:53:04 +0800

    no message

--- js/src/main/scala/web/view/QuickSearchDialog.scala
+++ js/src/main/scala/web/view/QuickSearchDialog.scala
@@ -29,8 +29,9 @@ class QuickSearchDialog(val client: Client,
   ).render
 
 
-  private val special = Seq("!h1", "!h2", "!h3", "!h4", "!h5", "!h6", "!heading", "!code") ++ delimitationGraphemes.values.toSet[model.data.Unicode].toSeq.map(_.str)
-  private val specialDesc = s"Special commands: ${special.mkString(" ")}"
+  private val special = Seq("!h1", "!h2", "!h3", "!h4", "!h5", "!h6", "!heading", "!code")
+  private val marks = delimitationGraphemes.values.toSet[model.data.Unicode].toSeq.map(_.str)
+  private val specialDesc = s"Special commands: ${(special ++ marks).mkString(" ")}"
 
 
   override protected def headerSize: Int = 1
