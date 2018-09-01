57c4208ab85202b3808cc9a2a39d207be33ddfb3
Minghao Liu <molikto@gmail.com>
2018-09-01 19:47:20 +0800

    what

--- shared/src/main/scala/model/package.scala
+++ shared/src/main/scala/model/package.scala
@@ -18,7 +18,7 @@ package object model extends Picklers {
   val debug_scroll = debug_view && true
   var debug_model = true
   val debug_transmit = false
-  val debug_selection = debug_view && true
+  val debug_selection = debug_view && false
 
   var debug_RenderEmptyInsertionPointAsBox = false
 
